5b97d1b99d77ee55c8c4fc1e62dfddf61a56f023
Mati Joseph <crucialniccur@gmail.com>
2025-05-08 05:51:40 +0300

    do the get_names test

--- lib/data_structures.py
+++ lib/data_structures.py
@@ -16,23 +16,30 @@ spicy_foods = [
     },
 ]
 
+
 def get_names(spicy_foods):
-    pass
+    return [food['name'] for food in spicy_foods]
+
 
 def get_spiciest_foods(spicy_foods):
     pass
 
+
 def print_spicy_foods(spicy_foods):
     pass
+
 
 def get_spicy_food_by_cuisine(spicy_foods, cuisine):
     pass
 
+
 def print_spiciest_foods(spicy_foods):
     pass
 
+
 def get_average_heat_level(spicy_foods):
     pass
+
 
 def create_spicy_food(spicy_foods, spicy_food):
     pass
